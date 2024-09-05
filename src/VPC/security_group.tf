0b8c05e2b57e2c2897678925cbd03040d9435cd1
Josewp10 <jdmowp10@gmail.com>
2024-09-05 12:01:34 -0500

    Security group deletion prevention

--- src/VPC/security_group.tf
+++ src/VPC/security_group.tf
@@ -6,12 +6,34 @@ resource "aws_security_group" "allow_tls" {
   tags = {
     Name = var.vpc_sg_name
   }
+  lifecycle {
+    prevent_destroy = false
+  }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = var.sg_cidr
+resource "aws_security_group_rule" "allow_tls_https" {
+  type              = "ingress"
   from_port         = 443
-  ip_protocol       = "tcp"
   to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = [var.sg_cidr]
+  security_group_id = aws_security_group.allow_tls.id
+}
+
+resource "aws_security_group_rule" "allow_tls_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = [var.sg_cidr]
+  security_group_id = aws_security_group.allow_tls.id
+}
+
+resource "aws_security_group_rule" "allow_all_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"] # Allows traffic to anywhere
+  security_group_id = aws_security_group.allow_tls.id
 }
